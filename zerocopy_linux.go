d66e4c1c8c129a96320680e02665676519243578
Andrei Tudor Călin <mail@acln.ro>
2019-03-31 04:01:55 +0200

    introduce new transfer algorithm
    Avoid potential deadlocks in splice(2) and tee(2) operations.
    
    Signed-off-by: Andrei Tudor Călin <mail@acln.ro>

--- zerocopy_linux.go
+++ zerocopy_linux.go
@@ -14,6 +14,119 @@
 
 package zerocopy
 
+/*
+On Linux, package zerocopy uses splice(2) and tee(2) to accelerate I/O
+pipelines. While most I/O system calls operate on a single file descriptor,
+splice(2) and tee(2) operate on two. This makes using them from Go quite
+tricky. Special precautions have to be taken in order to avoid a certain
+class of deadlocks, and play nice with the outside world.
+
+The algorithm to acheive this is rather complicated and subtle, but it is
+used in a number of places in this package, with minor adaptations based on
+context. This comment describes the algorithm. Because the real Go code that
+implements the algorithm is much more messy, and involves more book-keeping
+in terms of error handling, the algorithm is presented in pseudo-code.
+
+Preparatory definitions
+-----------------------
+
+increfscope takes a file descriptor argument, acquires a reference to
+descritpro, runs the block of code, then releases the reference when control
+exits the block (either naturally or via a goto statement).
+
+transfer is a data transfer function, either splice(2) or tee(2).
+
+wait waits for its file descriptor argument to be ready for an operation,
+either 'r' for reading, or 'w' for writing.
+
+
+The algorithm
+-------------
+
+	transfered = 0
+	errno = nil
+
+	round1:
+	increfscope(rfd) {
+		readready = false
+
+		round1again:
+		increfscope(wfd) {
+			transfered, errno = transfer(rfd, wfd)
+			if errno is EAGAIN {
+				if readready {
+					goto round2
+				}
+			} else {
+				goto end
+			}
+		}
+
+		wait(rfd, 'r')
+		readready = true
+		goto round1again
+	}
+
+	round2:
+	increfscope(wfd) {
+		writeready = false
+
+		round2again:
+		increfscope(rfd) {
+			transfered, errno = transfer(rfd, wfd)
+			if errno is EAGAIN {
+				if writeready {
+					goto round1
+				}
+			} else {
+				goto end
+			}
+		}
+
+		wait(wfd, 'w')
+		writeready = true
+		goto round2again
+	}
+
+	end:
+	return transfered, errno
+
+Restrictions, Analysis
+----------------------
+
+transfer can only be called while holding a reference to both file
+descriptors. wait can only be called while holding a reference to the file
+descriptor. It is forbidden to call wait on a file descriptor while also
+holding a reference to another file descriptor.
+
+Consider the naïve try:
+
+increfscope(rfd) {
+	increfscope(wfd) {
+		again:
+		transfered, errno = transfer(rfd, wfd)
+		if errno is EAGAIN {
+			wait(rfd, 'r')
+			wait(wfd, 'w')
+			goto again
+		}
+	}
+}
+
+This snippet is broken and dangerous.
+
+If we're blocked in wait(wfd, 'w'), and in another place, a caller tries to
+perform any operation on rfd, such as a hypothetical close(rfd), then the
+other caller is blocked, waiting for an unrelated operation, on another
+file descriptor.
+
+The algorithm is so complicated because has to take great care to not end up
+in a situation equivalent to this snippet. See also golang.org/issues/25985
+for an example of such a bug, from the stdlib splice implementation.
+
+Any changes to this package must retain these properties.
+*/
+
 import (
 	"io"
 	"os"
@@ -87,41 +200,78 @@ func (p *Pipe) read(b []byte) (int, error) {
 	// Hopefully this approach is good enough for general use. Doing
 	// anything else would be exceptionally complicated, and would require
 	// the library to be either very configurable, or very opinionated.
+	//
+	// HC SVNT DRACONES. See the comment at the top of the file.
 	var (
 		copied int64
-		werr   error
-		wrcerr error
+		operr  error // error from tee(2)
+		rrcerr error // non-nil if read FD is dead
+		wrcerr error // non-nil if write FD is dead
+
+		waitread      = false
+		readready     = false
+		done          = false
+		writeready    = false
+		waitwrite     = false
+		waitreadagain = false
 	)
-	readready := false
-	waitread := false
-	p.rrc.Read(func(prfd uintptr) bool {
+again:
+	rrcerr = p.rrc.Read(func(prfd uintptr) bool {
 		wrcerr = p.teepipe.wrc.Write(func(pwfd uintptr) bool {
-			copied, werr = tee(prfd, pwfd, len(b))
-			if werr == unix.EAGAIN {
+			copied, operr = tee(prfd, pwfd, len(b))
+			if operr == unix.EAGAIN {
 				if !readready {
 					waitread = true
-					return true
 				}
-				return false
+				return true
 			}
-			werr = os.NewSyscallError("tee", werr)
+			done = true
+			operr = os.NewSyscallError("tee", operr)
 			return true
 		})
 		if waitread {
-			// The next time we enter this function, we will
-			// be ready to read.
 			readready = true
 			waitread = false
 			return false
 		}
 		return true
 	})
-	// We are deliberately ignoring the error from this last call to
-	// p.rrc.Read: a Read on a syscall.RawConn only returns an error
-	// if the file descriptor is closed. If the read side of the pipe
-	// we own is indeed closed, the next call to Read on p.teerd will
-	// observe this condition. In that case, we let the better error
-	// reporting of package os kick in.
+	if rrcerr != nil || done {
+		goto end
+	}
+	wrcerr = p.teepipe.wrc.Write(func(pwfd uintptr) bool {
+		p.rrc.Read(func(prfd uintptr) bool {
+			copied, operr = tee(prfd, pwfd, len(b))
+			if operr == unix.EAGAIN {
+				if writeready {
+					waitreadagain = true
+				} else {
+					waitwrite = true
+				}
+				return true
+			}
+			operr = os.NewSyscallError("tee", operr)
+			return true
+		})
+		if waitwrite {
+			writeready = true
+			waitwrite = false
+			return false
+		}
+		return true
+	})
+	if wrcerr != nil {
+		goto end
+	}
+	if waitreadagain {
+		goto again
+	}
+end:
+	// If rrcerr is not nil, we do not report it immediately: a Read on
+	// a syscall.RawConn only returns an error if the file descriptor
+	// is closed. If the read side of the pipe we own is indeed closed,
+	// the next call to Read on p.teerd will observe this condition. In
+	// that case, we let the better error reporting of package os kick in.
 	//
 	// As for write errors on the pipe we tee to, if the target FD is
 	// closed, then the pipeline is dead anyway. All we've done so far
@@ -131,7 +281,7 @@ func (p *Pipe) read(b []byte) (int, error) {
 	// io.TeeReader does as well.
 	//
 	// Finally, we must be careful not to read more than we copied to
-	// the other pipe, otherwise we will have missed data.
+	// the other pipe, otherwise we will have missed tee-ing some data.
 	limit := len(b)
 	if copied > 0 {
 		limit = int(copied)
@@ -139,6 +289,9 @@ func (p *Pipe) read(b []byte) (int, error) {
 	n, err := p.teerd.Read(b[:limit])
 	if wrcerr != nil {
 		return n, wrcerr
+	}
+	if operr != nil {
+		return n, operr
 	}
 	return n, err
 }
@@ -162,7 +315,7 @@ func (p *Pipe) readFrom(src io.Reader) (int64, error) {
 	if !ok {
 		return io.Copy(p.w, src)
 	}
-	rc, err := sc.SyscallConn()
+	rrc, err := sc.SyscallConn()
 	if err != nil {
 		return io.Copy(p.w, src)
 	}
@@ -170,76 +323,111 @@ func (p *Pipe) readFrom(src io.Reader) (int64, error) {
 	var (
 		atEOF  bool
 		moved  int64
-		werr   error
+		operr  error
+		rrcerr error
 		wrcerr error
+
+		fallback      = false
+		waitread      = false
+		readready     = false
+		writeready    = false
+		waitwrite     = false
+		waitreadagain = false
 	)
 again:
-	readready := false
-	waitread := false
-	fallback := false
+	ok = false
 	max := maxSpliceSize
 	if int64(max) > limit {
 		max = int(limit)
 	}
-	err = rc.Read(func(rfd uintptr) bool {
+	rrcerr = rrc.Read(func(rfd uintptr) bool {
 		wrcerr = p.wrc.Write(func(pwfd uintptr) bool {
 			var n int
-			n, werr = splice(rfd, pwfd, max)
+			n, operr = splice(rfd, pwfd, max)
 			limit -= int64(n)
 			moved += int64(n)
-			if werr == unix.EINVAL {
+			if operr == unix.EINVAL {
 				fallback = true
 				return true
 			}
-			if werr == unix.EAGAIN {
+			if operr == unix.EAGAIN {
 				if !readready {
 					waitread = true
-					return true
 				}
-				return false
+				return true
 			}
-			if n == 0 && werr == nil {
-				atEOF = true
+			if operr == nil {
+				if n == 0 {
+					atEOF = true
+				} else {
+					ok = true
+				}
 			}
-			werr = os.NewSyscallError("splice", werr)
+			operr = os.NewSyscallError("splice", operr)
 			return true
 		})
-		if wrcerr != nil {
-			return true
-		}
 		if fallback {
 			return true
 		}
 		if waitread {
-			// The next time we enter this function, we will
-			// be ready to read.
 			readready = true
 			waitread = false
 			return false
-		}
-		if werr != nil {
-			return true
 		}
 		return true
 	})
 	if fallback {
 		return io.Copy(p.w, src)
 	}
-	if err != nil {
-		return moved, err
+	if wrcerr != nil || atEOF {
+		return moved, wrcerr
+	}
+	if ok {
+		if limit > 0 {
+			goto again
+		}
+		goto end
+	}
+
+	// If we're here, we have not spliced yet on this round, and we're
+	// waiting for the pipe to be ready.
+	wrcerr = p.wrc.Write(func(pwfd uintptr) bool {
+		rrcerr = rrc.Read(func(rfd uintptr) bool {
+			var n int
+			n, operr = splice(rfd, pwfd, max)
+			limit -= int64(n)
+			moved += int64(n)
+			if operr == unix.EAGAIN {
+				if writeready {
+					waitreadagain = true
+				} else {
+					waitwrite = true
+				}
+				return true
+			}
+			operr = os.NewSyscallError("splice", operr)
+			return true
+		})
+		if waitwrite {
+			writeready = true
+			waitwrite = false
+			return false
+		}
+		return true
+	})
+	if rrcerr != nil {
+		return moved, rrcerr
 	}
 	if wrcerr != nil {
 		return moved, wrcerr
 	}
-	if werr != nil {
-		return moved, werr
+	if operr != nil {
+		return moved, operr
 	}
-	if atEOF {
-		return moved, nil
-	}
-	if limit > 0 {
+	if limit > 0 || waitreadagain {
 		goto again
 	}
+end:
 	return moved, nil
 }
 
@@ -248,7 +436,7 @@ func (p *Pipe) writeTo(dst io.Writer) (int64, error) {
 	if !ok {
 		return io.Copy(dst, onlyReader{p})
 	}
-	rc, err := sc.SyscallConn()
+	wrc, err := sc.SyscallConn()
 	if err != nil {
 		return io.Copy(dst, onlyReader{p})
 	}
@@ -256,63 +444,103 @@ func (p *Pipe) writeTo(dst io.Writer) (int64, error) {
 	var (
 		atEOF  bool
 		moved  int64
-		werr   error
+		operr  error
+		rrcerr error
 		wrcerr error
+
+		fallback      = false
+		waitread      = false
+		readready     = false
+		writeready    = false
+		waitwrite     = false
+		waitreadagain = false
 	)
 again:
-	readready := false
-	waitread := false
-	fallback := false
-	err = p.rrc.Read(func(prfd uintptr) bool {
-		wrcerr = rc.Write(func(wfd uintptr) bool {
+	ok = false
+	rrcerr = p.rrc.Read(func(rfd uintptr) bool {
+		wrcerr = wrc.Write(func(pwfd uintptr) bool {
 			var n int
-			n, werr = splice(prfd, wfd, maxSpliceSize)
+			n, operr = splice(rfd, pwfd, maxSpliceSize)
 			moved += int64(n)
-			if werr == unix.EINVAL {
+			if operr == unix.EINVAL {
 				fallback = true
 				return true
 			}
-			if werr == unix.EAGAIN {
+			if operr == unix.EAGAIN {
 				if !readready {
 					waitread = true
-					return true
 				}
-				return false
+				return true
 			}
-			if n == 0 && werr == nil {
-				atEOF = true
+			if operr == nil {
+				if n == 0 {
+					atEOF = true
+				} else {
+					ok = true
+				}
 			}
-			werr = os.NewSyscallError("splice", werr)
+			operr = os.NewSyscallError("splice", operr)
 			return true
 		})
+		if fallback {
+			return true
+		}
 		if waitread {
-			// The next time we enter this function, we will
-			// be ready to read.
 			readready = true
 			waitread = false
 			return false
 		}
-		if werr != nil {
-			return true
-		}
 		return true
 	})
 	if fallback {
-		return io.Copy(dst, p.teerd)
+		return io.Copy(dst, onlyReader{p})
 	}
-	if err != nil {
-		return moved, err
+	if wrcerr != nil || atEOF {
+		return moved, wrcerr
+	}
+	if ok {
+		goto end
+	}
+
+	// If we're here, we have not spliced yet on this round, and we're
+	// waiting for the destination file descriptor to be ready.
+	wrcerr = wrc.Write(func(pwfd uintptr) bool {
+		rrcerr = p.rrc.Read(func(rfd uintptr) bool {
+			var n int
+			n, operr = splice(rfd, pwfd, maxSpliceSize)
+			moved += int64(n)
+			if operr == unix.EAGAIN {
+				if writeready {
+					waitreadagain = true
+				} else {
+					waitwrite = true
+				}
+				return true
+			}
+			operr = os.NewSyscallError("splice", operr)
+			return true
+		})
+		if waitwrite {
+			writeready = true
+			waitwrite = false
+			return false
+		}
+		return true
+	})
+	if rrcerr != nil {
+		return moved, rrcerr
 	}
 	if wrcerr != nil {
 		return moved, wrcerr
 	}
-	if werr != nil {
-		return moved, werr
+	if operr != nil {
+		return moved, operr
 	}
-	if atEOF {
-		return moved, nil
+	if waitreadagain {
+		goto again
 	}
-	goto again
+end:
+	return moved, nil
 }
 
 func transfer(dst io.Writer, src io.Reader) (int64, error) {
